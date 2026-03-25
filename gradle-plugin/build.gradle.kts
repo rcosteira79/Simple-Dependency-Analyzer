234ccb9d253cb02aa40e42332968f914ba9431cf
Ricardo Costeira <ricardo.costeira@mindera.com>
2026-03-25 14:42:18 +0100

    fix: align serialization plugin version with kotlin-dsl, add gitignore

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("plugin.serialization") version "1.9.22"
     id("com.gradle.plugin-publish") version "1.2.1"
 }
 
@@ -10,6 +10,7 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
